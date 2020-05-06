eba3b220762855c95d31bcd950c5087d2b768a56
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-05-07 00:03:29 +0900

    change spinner color
    change spinner color by using 0xff

--- app/src/main/java/com/dontsu/dogs2/util/Util.kt
+++ app/src/main/java/com/dontsu/dogs2/util/Util.kt
@@ -7,8 +7,11 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.dontsu.dogs2.R
 
+const val ORANGE = 0xFFFDBF43 //0xFF : 16진수
+
 fun getProgressDrawable(context: Context): CircularProgressDrawable { //이미지 로딩시 스피너 보여주기
     return CircularProgressDrawable(context).apply {
+        colorSchemeColors[0] = ORANGE.toInt()//스피너 색 변경
         strokeWidth = 10f
         centerRadius = 50f
         start()
