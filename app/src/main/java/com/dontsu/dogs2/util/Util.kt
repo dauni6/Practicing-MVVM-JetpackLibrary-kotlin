ff6e0e5c8e9ebcd42292db091234445af565806d
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-05-13 21:32:40 +0900

    dataBinding for dogdata
    using fun, data

--- app/src/main/java/com/dontsu/dogs2/util/Util.kt
+++ app/src/main/java/com/dontsu/dogs2/util/Util.kt
@@ -2,6 +2,7 @@ package com.dontsu.dogs2.util
 
 import android.content.Context
 import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -26,4 +27,9 @@ fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable
         .setDefaultRequestOptions(options)
         .load(uri)
         .into(this) //this는 현재 ImageView
+}
+
+@BindingAdapter("android:imageUrl")
+fun loadImage(view: ImageView, url: String?) {
+    view.loadImage(url, getProgressDrawable(view.context))
 }
--- app/src/main/java/com/dontsu/dogs2/view/adapters/DogListAdapter.kt
+++ app/src/main/java/com/dontsu/dogs2/view/adapters/DogListAdapter.kt
@@ -3,9 +3,11 @@ package com.dontsu.dogs2.view.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.dontsu.dogs2.R
+import com.dontsu.dogs2.databinding.ItemDogBinding
 import com.dontsu.dogs2.model.DogBreed
 import com.dontsu.dogs2.util.getProgressDrawable
 import com.dontsu.dogs2.util.loadImage
@@ -20,9 +22,13 @@ class DogListAdapter(private val dogsList: ArrayList<DogBreed>): RecyclerView.Ad
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =  DogViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.item_dog, parent, false)
-    )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):  DogViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        //val view = inflater.inflate(R.layout.item_dog, parent, false) data binding 안 쓸 때
+        val view = DataBindingUtil.inflate<ItemDogBinding>(inflater, R.layout.item_dog, parent, false)
+        return DogViewHolder(view)
+    }
+
 
     override fun getItemCount(): Int = dogsList.size
 
@@ -30,8 +36,10 @@ class DogListAdapter(private val dogsList: ArrayList<DogBreed>): RecyclerView.Ad
         holder.bind(dogsList[position])
     }
 
-    inner class DogViewHolder(var view: View): RecyclerView.ViewHolder(view) {
+    inner class DogViewHolder(var view: ItemDogBinding): RecyclerView.ViewHolder(view.root) {
         fun bind(dog: DogBreed) {
+            /*
+            //data binding 안 쓸 때
             view.name.text = dog.dogBreed
             view.lifespan.text = dog.lifespan
             view.setOnClickListener {
@@ -39,7 +47,8 @@ class DogListAdapter(private val dogsList: ArrayList<DogBreed>): RecyclerView.Ad
                 action.dogUuid = dog.uuid //uuid 정보를 갖고 DetailFragment로 이동
                 Navigation.findNavController(it).navigate(action)
             }
-            view.imageView.loadImage(dog.imageUrl, getProgressDrawable(view.imageView.context))
+            view.imageView.loadImage(dog.imageUrl, getProgressDrawable(view.imageView.context))*/
+            view.dog = dog //view가 ItemDogBinding 이고 item_dog.xml에 결합되어 있어 데이터를 자동으로 바꿔줌
         }
     }
 
