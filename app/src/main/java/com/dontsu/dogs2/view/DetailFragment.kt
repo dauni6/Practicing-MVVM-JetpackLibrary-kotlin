1c484c6c34cdd381f0b20af2c2cf579bdbb52b47
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-05-13 22:18:17 +0900

    detailFragment databinding
    detailFragment databinding

--- app/src/main/java/com/dontsu/dogs2/view/DetailFragment.kt
+++ app/src/main/java/com/dontsu/dogs2/view/DetailFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 
 import com.dontsu.dogs2.R
+import com.dontsu.dogs2.databinding.FragmentDetailBinding
 import com.dontsu.dogs2.model.DogBreed
 import com.dontsu.dogs2.util.getProgressDrawable
 import com.dontsu.dogs2.util.loadImage
@@ -18,12 +20,14 @@ import kotlinx.android.synthetic.main.fragment_detail.*
 class DetailFragment : Fragment() {
 
     private lateinit var viewModel: DetailViewModel
+    private lateinit var dataBinding: FragmentDetailBinding
 
     private var dogUuid = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        dataBinding = DataBindingUtil.inflate<FragmentDetailBinding>(inflater, R.layout.fragment_detail, container, false)
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -42,11 +46,13 @@ class DetailFragment : Fragment() {
     private fun observeViewModel() {
         viewModel.dogLiveData.observe(this, Observer {dog: DogBreed ->
             dog?.let {
-                detail_dogName.text = dog.dogBreed
+               /* data biding 쓰기전 코드
+               detail_dogName.text = dog.dogBreed
                 detail_dogLifespan.text = dog.lifespan
                 detail_dogTemperament.text = dog.temperament
                 detail_dogPurpose.text= dog.bredFor
-                context?.let { detail_dogImage.loadImage(dog.imageUrl, getProgressDrawable(it)) }
+                context?.let { detail_dogImage.loadImage(dog.imageUrl, getProgressDrawable(it)) }*/
+                dataBinding.dog = dog
             }
         })
     }
